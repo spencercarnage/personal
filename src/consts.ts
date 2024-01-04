023a19b6c86285455c4b3fd103e5c4035c476dbd
spencercarnage <spencercarnage@gmail.com>
2024-01-04 09:51:13 -0800

    update styles of site

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Spencer Carney's Blog";
+export const SITE_TITLE = "Spencer Carney";
 export const SITE_DESCRIPTION =
   "A blog about a front-end developer who found his way to management";
